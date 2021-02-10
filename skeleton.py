9978529f79542aa120818ae0f6f004d1bd0e6965
Alberto Gori <a.gori@student.tudelft.nl>
2021-02-10 19:13:19 +0100

    Nothing changed

--- skeleton.py
+++ skeleton.py
@@ -8,7 +8,7 @@ import numpy as np
 
 
 def simulate(init_pos, init_vel, num_tsteps, timestep, box_dim):
-    """
+    """"
     Molecular dynamics simulation using the Euler or Verlet's algorithms
     to integrate the equations of motion. Calculates energies and other
     observables at each timestep. HELOOOOOO
