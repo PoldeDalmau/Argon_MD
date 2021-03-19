a2c2429213e3690d45e7b4d4fe24b78c28eb46de
Alberto Gori <a.gori@student.tudelft.nl>
2021-03-19 13:28:59 +0100

    Improved performance removing for loops where possible. Error and rescaling still need work.

--- skeleton.py
+++ skeleton.py
@@ -121,6 +121,28 @@ def fcc_lattice(a):
     fcc_positions = fcc_positions[:n,:]  #picks n particles out of all,     
     return (fcc_positions)
 
+def init_velocity(T, n):
+    """
+    Initializes the initial velocities of the particles.
+    
+    Parameters
+    ----------
+    T : float
+        Initial temperature of the system
+    n : integer
+        Number of particles inside the system
+    
+    returns
+    -------
+    init_vel : np.ndarray
+        nx3 array containing the initial velocities of the particles. Each column represents an axis, each row a different particle
+    """
+    init_vel = np.zeros((n,3))
+    init_vel = np.random.normal(np.sqrt(T/119.8), size = (n, 3))
+    #init_vel = init_vel - np.mean(init_vel, axis=0)
+    
+    return init_vel
+
 
 def atomic_distances(pos, output): # output = 0 gives the relative distances, output = 1 gives the relative positions
     """
@@ -128,7 +150,7 @@ def atomic_distances(pos, output): # output = 0 gives the relative distances, ou
 
     parameters
     pos : np.ndarray
-        The positions of the particles in cartesian space.
+        The positions of the particles in cartesian space at the latest timestep.
     output: float
         If output = 0 the function returns the relative distances between the particles.
         If output = 1 the function returns the relative positions of the particles. 
@@ -158,14 +180,16 @@ def atomic_distances(pos, output): # output = 0 gives the relative distances, ou
         
 #compute the forces on the particles at each timestep
 
-def lj_force(position):
+def lj_force(rel_pos, r):
     """
     Calculates the net forces on each atom.
 
     Parameters
     ----------
-    position : np.ndarray
-        Relative particle positions as obtained from atomic_distances.
+    rel_pos : np.ndarray
+        Relative particle positions at the latest timestep.
+    r : np.ndarray
+        Relative particle distances at the latest timestep.
     
 
     Returns
@@ -173,15 +197,13 @@ def lj_force(position):
     np.ndarray
         The net force acting on particle i due to all other particles.
     """
-    rel_pos = atomic_distances(position[:,-3:], 1) # calculates the relative position of the particles, with pos = np.array(N_particels, 3)
-    r = atomic_distances(position[:,-3:], 0) # n x n simmetric matrix, r[i,j] is the distance between the i-th and the j-th particles
-    R = np.zeros((n,n,3))
-    for i in range(n):
-        for j in range (n):
-            for k in range (3):
-                R[i,j,k] = r [i,j]
-                if r[i,j] == 0:
-                    R[i,j,k] = np.inf
+    #rel_pos = atomic_distances(position[:,-3:], 1) # calculates the relative position of the particles, with pos = np.array(N_particels, 3)
+    #r = atomic_distances(position[:,-3:], 0) # n x n simmetric matrix, r[i,j] is the distance between the i-th and the j-th particles
+    zeros = np.zeros((n,n))
+    np.fill_diagonal(zeros, np.inf)         # nxn diagonal matrix with np.inf on the diagonal
+    r = r + zeros
+    temp = np.reshape(r, (n,n,1))           # temporary array to build R in the right shape (n,n,3)
+    R = np.concatenate((temp, temp, temp), axis=2)
     # R are n matricies in the form nx3, where the i-th matrix contains the distances from the i-th particle, calling R_i the i-th matrix, 
     # R_i[j,k] gives the distance between the i-th and the j-th particles and is the same value for all the 'k in range(3)', i.e. on all the j-th line
     # in addition, R_i[i,k] (i-th line of the i-th matrix) is equal to 1 instead of 0 in order not to divide by 0 in the computation of F
@@ -193,21 +215,51 @@ def lj_force(position):
     
     
     
-def euler(final_matrix_pos, final_matrix_vel):
-    latest_pos = np.copy(final_matrix_pos[:,-3:])# -3 is for the dimensions
-    latest_vel = np.copy(final_matrix_vel[:,-3:])
+def euler(latest_pos, latest_vel, rel_pos, rel_dist):
+    """
+    Updates the positions of each particle at each timestep using Euler algorithm.
+    
+    Parameters
+    ----------
+    latest_pos : np.ndarray
+        Matrix with the positions of the particles at the latest timestep
+    latest_vel : np.ndarray
+        Matrix with the velocities of the particles at the latest timestep
+        
+    Return
+    ------
+    np.ndarray
+        Returns two arrays: one containing the postions, and one containing the velocities at the latest timestep
+    """
+    #latest_pos = np.copy(final_matrix_pos[:,-3:])# -3 is for the dimensions
+    #latest_vel = np.copy(final_matrix_vel[:,-3:])
     
     new_latest_pos = latest_pos + latest_vel * time_step
-    new_latest_vel = latest_vel + time_step * lj_force(latest_pos)
+    new_latest_vel = latest_vel + time_step * lj_force(rel_pos, rel_dist)
     return new_latest_pos, new_latest_vel
 
-def verlet(final_matrix_pos, final_matrix_vel):
+def verlet(latest_pos, latest_vel, rel_pos, rel_dist):
+    """
+    Updates the positions of each particle at each timestep using Verlet algorithm.
     
-    latest_pos = np.copy(final_matrix_pos[:,-3:])# -3 is for the dimensions
-    latest_vel = np.copy(final_matrix_vel[:,-3:])
+    Parameters
+    ----------
+    latest_pos : np.ndarray
+        Matrix with the positions of the particles at the latest timestep
+    latest_vel : np.ndarray
+        Matrix with the velocities of the particles at the latest timestep
+        
+    Return
+    ------
+    np.ndarray
+        Returns two arrays: one containing the postions, and one containing the velocities at the latest timestep
+    """
     
-    new_latest_pos = latest_pos + time_step * latest_vel + (1/2) * time_step**2  * lj_force(latest_pos)
-    new_latest_vel = latest_vel + (time_step/2) * (lj_force(new_latest_pos) + lj_force(latest_pos))
+    #latest_pos = np.copy(final_matrix_pos[:,-3:])# -3 is for the dimensions
+    #latest_vel = np.copy(final_matrix_vel[:,-3:])
+    
+    new_latest_pos = latest_pos + time_step * latest_vel + (1/2) * time_step**2  * lj_force(rel_pos, rel_dist)
+    new_latest_vel = latest_vel + (time_step/2) * (lj_force(rel_pos, rel_dist) + lj_force(rel_pos, rel_dist))
     return new_latest_pos, new_latest_vel
 
 
@@ -230,22 +282,22 @@ def kin_en(v): #v is the last step velocity
     return K
     
 #function to compute the potential energy
-def pot_en(position): #position is the last step position of the particles
+def pot_en(r): #position is the last step position of the particles
     """
     Computes the potential energy of an atomic system.
 
     Parameters
     ----------
-    position : np.ndarray
-        Particle positions as obtained from atomic_distances
+    r : np.ndarray
+        Particles relative distances at the latest timestep.
 
     Returns
     -------
     float
         The total potential energy of the system.
     """
-    rel_pos = atomic_distances(position, 1) # with pos = np.array(n, 3)
-    r = atomic_distances(position, 0) #n x n simmetric matrix, r[i,j] is the distance between the i-th and the j-th particles
+    #rel_pos = atomic_distances(position, 1) # with pos = np.array(n, 3)
+    #r = atomic_distances(position, 0) #n x n simmetric matrix, r[i,j] is the distance between the i-th and the j-th particles
     i = np.where(r == 0) # indices where the r matrix is 0
     ones = np.zeros((n,n)) # nxn matrix
     ones[i] = 1 # matrix with ones where r has zeros
@@ -255,21 +307,54 @@ def pot_en(position): #position is the last step position of the particles
     return U
 
 #function to compute the pressure
-def pressure(position): #position is the matrix with all the positions stored in it
+def pressure(r): 
+    """
+    Computes the value that will be averaged to find the pressure
     
-    r = atomic_distances(position[:,-3:], 0) #n x n simmetric matrix, r[i,j] is the distance between the i-th and the j-th particles
+    Parameters
+    ----------
+    r : np.ndarray
+        Particles relative distances at the latest timestep.
+
+    Returns
+    -------
+    float
+        Returns the sum of the element of the matrix, which is the value that will be averaged to find the pressure.
+    """
+    
+    #r = atomic_distances(position[:,-3:], 0) #n x n simmetric matrix, r[i,j] is the distance between the i-th and the j-th particles
     i = np.where(r == 0) # indices where the r matrix is 0
-    ones = np.zeros((n,n)) # nxn matrix
-    ones[i] = 1 # matrix with ones where r has zeros
-    R = r + ones  # same matrix as r, but with ones where r has zeros. we do this to avoid dividing by zero
+    inf = np.zeros((n,n)) # nxn matrix
+    inf[i] = np.inf # matrix with inifinity where r has zeros
+    R = r + inf  # same matrix as r, but with ones where r has zeros. we do this to avoid dividing by zero
     tba_matrix = -12*((2*(1/R)**12)-((1/R)**6))
     tba = np.sum(tba_matrix)
     #rho = n/(L*L*L)
     #P = (rho*119.8/T) - (rho/(3*n))*
     return tba
+
+def pair_correlation(rel_dist, dr):
+    """
+    Calculates pair correlation for all particles at a given time.
+    Parameters
+    ----------
+    rel_dist : np.ndarray
+        relative distances between atoms as calculated by atomic_distances()
+    Returns
+    -------
+    Fills a histogram.
+        
+    """
+
+    rel_dist = np.reshape(rel_dist, (n*n))
+    bins_list = np.arange(dr, L+dr, dr)
+    counts = pd.cut(rel_dist, bins_list, include_lowest = True)
+    counts= counts.value_counts()
+    counts = counts.to_numpy()
+    return counts/2 #avoids double counting
     
     
-def simulate(algorithm, rescaling_bool, pressure_bool, error_bool):
+def simulate(algorithm, rescaling_bool, pressure_bool, error_bool, pair_correlation_bool):
     """
     Molecular dynamics simulation using the Euler or Verlet's algorithms
     to integrate the equations of motion. Calculates energies and other
@@ -293,22 +378,25 @@ def simulate(algorithm, rescaling_bool, pressure_bool, error_bool):
     Any quantities or observables that you wish to study.
     """
         
+    T = 1.35
+    #Create a nx3 matrix to store the velocity of each particle at each step in time.
+    next_step_velocity = np.copy(init_velocity(T, n))
 
-    #Create a 2x8 matrix to store the velocity of each particle at each step in time.
-    next_step_velocity = np.copy(init_vel)
-
-    #Create a 2x8 matrix to store the position of each particle at each step in time.
+    #Create a nx3 matrix to store the position of each particle at each step in time.
     next_step_position = np.copy(init_pos)
     
     final_matrix_pos = np.copy(init_pos)
-    final_matrix_vel = np.copy(init_vel)
+    final_matrix_vel = np.copy(init_velocity(T, n))
     
-    final_vector_kin = np.array([kin_en(init_vel)])
-    final_vector_pot = np.array([pot_en(next_step_position)])
-    final_vector_energy = np.array([kin_en(init_vel) + pot_en(next_step_position)])
+    rel_dist = atomic_distances(init_pos, 0)
+    final_vector_kin = np.array([kin_en(init_velocity(T, n))])
+    final_vector_pot = np.array([pot_en(rel_dist)])
+    final_vector_energy = np.array([kin_en(init_velocity(T, n)) + pot_en(rel_dist)])
+    
+    final_vector_corr = pair_correlation(rel_dist, dr)
     
     if pressure_bool == True:
-        final_vector_tba = np.array([pressure(init_pos)])
+        final_vector_tba = np.array([pressure(atomic_distances(final_matrix_pos[:,-3:], 0))])
         final_vector_press = np.copy([final_vector_tba[0]])
         rho = n/(L*L*L)
     
@@ -318,18 +406,23 @@ def simulate(algorithm, rescaling_bool, pressure_bool, error_bool):
     j = 0
 
     for i in range(number_of_steps):
-        next_step_position, next_step_velocity = algorithm(final_matrix_pos, final_matrix_vel)
+        rel_pos = atomic_distances(final_matrix_pos[:,-3:], 1)
+        rel_dist = atomic_distances(final_matrix_pos[:,-3:], 0)
+        next_step_position, next_step_velocity = algorithm(final_matrix_pos[:,-3:], final_matrix_vel[:,-3:], rel_pos, rel_dist)
         next_step_position = next_step_position % L
         final_matrix_pos =  np.concatenate((final_matrix_pos, next_step_position), axis=1, out=None)
         final_matrix_vel =  np.concatenate((final_matrix_vel, next_step_velocity), axis=1, out=None)
         final_vector_kin = np.concatenate((final_vector_kin, np.array([kin_en(next_step_velocity)])), axis = 0, out = None)
-        final_vector_pot = np.concatenate((final_vector_pot, np.array([pot_en(next_step_position)])), axis=0, out=None)
-        final_vector_energy = np.concatenate((final_vector_energy, np.array([kin_en(next_step_velocity) + pot_en(next_step_position)])), axis=0, out=None)
+        final_vector_pot = np.concatenate((final_vector_pot, np.array([pot_en(rel_dist)])), axis=0, out=None)
+        final_vector_energy = np.concatenate((final_vector_energy, np.array([kin_en(next_step_velocity) + pot_en(rel_dist)])), axis=0, out=None)
         final_rel_dist = np.concatenate((final_rel_dist, atomic_distances(next_step_position, 0)), axis = 1, out = None)
         if pressure_bool == True:
             T = 2*kin_en(final_matrix_vel[:,-3:])/(3*(n-1)*119.8)
-            final_vector_tba = np.concatenate((final_vector_tba, np.array([pressure(final_matrix_pos)])), axis = 0, out = None)
+            final_vector_tba = np.concatenate((final_vector_tba, np.array([pressure(rel_dist)])), axis = 0, out = None)
             final_vector_press = np.concatenate((final_vector_press, np.array([np.sum([rho*119.8/T , -(rho/(3*n))*final_vector_tba[i]])])))
+        
+        if pair_correlation_bool:   # will add condition that it only computes this when i>n_0 or something
+            final_vector_corr += pair_correlation(rel_dist, dr)
         
         #Rescaling:
         if rescaling_bool ==True:
@@ -341,12 +434,16 @@ def simulate(algorithm, rescaling_bool, pressure_bool, error_bool):
                 #T = 2*kin_en(final_matrix_vel[:,-3:])/(3*(n-1)*119.8)
                 print("T = ", T)
     print("Number of rescalings: ", j)
+    
     # Compute pressure:
     if pressure_bool == True:
         n_0 = int(0.7*number_of_steps)
         #P = (1/(n-n_0))*np.sum(final_vector_tba[-n_0:])
-        P = np.sum([1 , -(T/119.8*(3*n))*(1/(number_of_steps-n_0))*np.sum(final_vector_tba[-n_0:])])
-        print("P=", P)
+        Press = np.sum([1 , -(T/119.8*(3*n))*(1/(number_of_steps-n_0))*np.sum(final_vector_tba[-n_0:])])
+        print("Pressure=", Press)
+        
+    if pair_correlation_bool:
+        final_vector_corr = final_vector_corr / number_of_steps     # number_of_steps should become n_0
 
     #print("Positions:\n" , final_matrix_pos)
     #print("Velocities:\n" , init_vel)
@@ -355,72 +452,51 @@ def simulate(algorithm, rescaling_bool, pressure_bool, error_bool):
     #print(final_vector_pot)
     #print(final_rel_dist)
 
+    
+    #now we compute the error with the steps below
+    if error_bool == True:
+        N = int(0.3*number_of_steps)
+        P = np.copy((final_vector_press[-N:]))
+        S_a = np.zeros((int(N/6), 1))
+        for b in range(1, int(N/6)):
+            Nb = int(N/b)
+            p=np.zeros(Nb)
+            P_shape=np.reshape(P, (Nb,b))
+            p = np.sum(P_shape, axis = 1)/b
+            S_a[b]=np.sqrt((1/(Nb-1))*(((1/Nb)*np.sum(np.square(p)))-np.square((1/Nb)*np.sum(p))))
 
-    print("")
-    plt.plot(times, final_vector_energy, label = "Total")
-    plt.plot(times, final_vector_kin, label = "Kinetic")
-    plt.plot(times, final_vector_pot, label = "Potential")
+        
+        
+    return final_vector_energy, final_vector_kin, final_vector_pot, final_vector_press, final_vector_corr, Press, S_a
+
+def plot_energy(total, kinetic, potential):
+    plt.plot(times, total, label = "Total")
+    plt.plot(times, kinetic, label = "Kinetic")
+    plt.plot(times, potential, label = "Potential")
     plt.legend()
     plt.title("Energy over time")
     plt.xlabel("$t/(m\sigma^2/\epsilon)^{1/2}$")
     plt.ylabel("$E/\epsilon$")
     plt.show()
     
+    
     #now we compute the error with the steps below
-    if error_bool == True:
-        b = 1
-        N = int(0.3*number_of_steps)
-        P = np.copy((final_vector_press[-n_0:]))
-        S_a = np.zeros((int(N/6), 1))
-        for b in range(1, int(N/6)):
-            Nb = int(N/b)
-            if Nb != 1:
-                p = np.zeros((Nb, 1))
-                for j in range(Nb):
-                    for i in range(j*b,(j+1)*b):
-                        p[j] += (1/b)*P[i+np.floor_divide(j,b)]
-                S_a[b]=np.sqrt((1/(Nb-1))*(((1/Nb)*np.sum(np.square(p)))-np.square((1/Nb)*np.sum(p))))
+def plot_error(S_a):
 
-        x = np.arange(0, len(S_a))
-        plt.plot(x, S_a)
-        plt.title("Error with data blocking")
-        plt.xlabel("b")
-        plt.ylabel("$\sigma_A$")
+    x = np.arange(0, len(S_a))
+    plt.plot(x, S_a)
+    plt.title("Error with data blocking")
+    plt.xlabel("b")
+    plt.ylabel("$\sigma_A$")
+    plt.show()
 
-
-
-
-#===================================================================================
-#we are keeping the initial functions to have the description available at all times
-#===================================================================================
-
-
-
-def simulate_empty(init_pos, init_vel, num_tsteps, timestep, box_dim): #arguments are global
-
-    """
-    Molecular dynamics simulation using the Euler or Verlet's algorithms
-    to integrate the equations of motion. Calculates energies and other
-    observables at each timestep.
-
-    Parameters
-    ----------
-    init_pos : np.ndarray
-        The initial positions of the atoms in Cartesian space
-    init_vel : np.ndarray
-        The initial velocities of the atoms in Cartesian space
-    num_tsteps : int
-        The total number of simulation steps
-    timestep : float
-        Duration of a single simulation step
-    box_dim : float
-        Dimensions of the simulation box
-
-    Returns
-    -------
-    Any quantities or observables that you wish to study.
-    """
-    
-    
-    
-    return 
+def plot_pair_correlation(vector_corr):
+    bins_list = np.arange(2*dr, L+2*dr, dr)
+    plt.title("pair correlation function")
+    plt.xlabel("r")
+    plt.ylabel("g(r)")
+    vector_corr = vector_corr[1:]
+    x = bins_list[:(len(vector_corr))] + dr/2
+    vector_corr = (2 * L**3 * vector_corr)/(4*np.pi* dr* n*(n-1)*x**2)
+    plt.plot(x, vector_corr)
+    plt.show()
